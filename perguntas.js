73cdc01cb9851af053a3f53db5d9fdf6e55fcf63
Amanda Lima <105943196+AmandAGstz@users.noreply.github.com>
2024-09-24 08:35:08 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro imperador de Roma',
+    'Augusto também conhecido como Otaviano').
+    
+    criaCartao(
+    
+    'História',
+    'Qual evento marcou o início da Idade Média?',
+    'A queda do Império Romano do Ocidente em 476 d.C.')
+    
+    criaCartao(
+    
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvares Cabral, em 1500.')
+    
+    criaCartao(
+    
+    'História',
+    'Qual foi a principal causa da Primeira Guerra Mundial?',
+    'O assassinato do arquiduque Francisco Ferdinando da Áustria.')
+    
+    criaCartao(
+    
+    'História',
+    'Quem liderou a Revolução Russa de 1917?',
+    'Vladimir Lênin.')
+    
+    criaCartao(
+    
+    'Filosofia',
+    'Quem é considerado o pai da filosofia?',
+    'Sócrates.')
+    
+    criaCartao(
+    
+    'Filosofia',
+    'Qual o principal conceito da filosofia de René Descartes?',
+    'Penso, logo existo.')
+    
+    criaCartao(
+    
+    'Filosofia',
+    'O que é o "mito da caverna" de Platão?',
+    'Uma metáfora sobre a ilusão e a busca pela verdade.')
+    
+    criaCartao(
+    
+    'Filosofia',
+    'Qual o conceito central do niilismo de Nietzsche?',
+    'A negação de valores e significados absolutos.')
+    
+    criaCartao(
+    
+    'Filosofia',
+    'O que é deontologia, segundo Immanuel Kant?',
+    'A ética baseada no dever e nas regras morais universais.')
+    
+    criaCartao(
+    
+    'Português',
+    'O que é um verbo transitivo?',
+    'Verbo que precisa de complemento para ter sentido completo.')
+    
+    criaCartao(
+    
+    'Português',
+    'O que é uma oração subordinada?',
+    'Oração que depende de outra para ter sentido.')
+    
+    criaCartao(
+    
+    'Português',
+    'Qual é o sujeito indeterminado?',
+    'Quando o sujeito não é identificado ou não pode ser determinado.')
+    
+    criaCartao(
+    
+    'Português',
+    'O que é um adjetivo?',
+    'Palavra que caracteriza o substantivo, indicando uma qualidade.')
+    
+    criaCartao(
+    
+    'Português',
+    'Qual a diferença entre "mais" e "mas"?',
+    'Mais indica quantidade; mas é usado para oposição conjunção adversativa.')
+    
+    criaCartao(
+    
+    'Biologia',
+    'O que são células procarióticas?',
+    'Células sem núcleo definido, como as bactérias.')
+    
+    criaCartao(
+    
+    'Biologia',
+    'Qual é a função dos ribossomos?',
+    'Síntese de proteínas.')
+    
+    criaCartao(
+    
+    'Biologia',
+    'O que é fotossíntese?',
+    'Processo pelo qual plantas produzem energia a partir da luz solar.')
+    
+    criaCartao(
+    
+    'Biologia',
+    'Qual a molécula responsável pelo transporte de oxigênio no sangue?',
+    'Hemoglobina.')
+    
+    criaCartao(
+    
+    'Biologia',
+    'O que são seres autotróficos?',
+    'Seres que produzem seu próprio alimento, como plantas.')
+    
